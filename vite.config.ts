463adf300e58e4611e9020cb998b5b67532ca7d2
Mouayad Saad <medosaad18@gmail.com>
2025-10-05 09:44:57 +0200

    last

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/my-showcase-portfolio/' : '/', // ← هذا هو المهم
+  base: mode === 'production' ? '/portfolio/' : '/', // ← هذا هو المهم
   server: {
     host: "::",
     port: 8080,
